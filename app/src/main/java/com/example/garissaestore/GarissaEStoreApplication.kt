bec85b316e9cd54e30d22738758a4fffa1ce8227
Anwar-alsina <Anwarfahad6700@gmail.com>
2022-10-14 23:37:55 +0300

    added Hilt for dependency injections

---
+++ app/src/main/java/com/example/garissaestore/GarissaEStoreApplication.kt
@@ -0,0 +1,13 @@
+package com.example.garissaestore
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GarissaEStoreApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        //Todo..
+    }
+}
---
+++ app/src/main/java/com/example/garissaestore/hilt/NetworkModule.kt
@@ -0,0 +1,46 @@
+package com.example.garissaestore.hilt
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.example.garissaestore.MainActivity
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.time.Duration
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun providesRetrofit(okHttpClient: OkHttpClient):Retrofit{
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("https://fakestoreapi.com/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(): OkHttpClient {
+        val duration = Duration.ofSeconds(30)
+            return OkHttpClient.Builder()
+                .connectTimeout(duration)
+                .readTimeout(duration)
+                .writeTimeout(duration)
+                .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesProductsService(retrofit: Retrofit): MainActivity.ProductsService{
+        return retrofit.create(MainActivity.ProductsService::class.java)
+
+    }
+}
